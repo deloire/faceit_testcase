f499f487e893e469b372a9a5addf835b50987633
Александр <lazzero@MacBook-Air.local>
2024-09-29 18:51:44 +0300

    Wrote a simple structure for FaceitTest class

--- src/test/java/com/faceit/FaceitTest.java
+++ src/test/java/com/faceit/FaceitTest.java
@@ -1,2 +1,14 @@
-package com.faceit;public class FaceitTest {
+package com.faceit;
+
+import org.junit.jupiter.api.Test;
+
+public class FaceitTest extends BaseClass {
+    private final static String URL = "https://www.faceit.com";
+    private final static String SEARCH_PLAYER = "s1mple";
+
+
+    @Test
+    public void checkPlayerStatistics() {
+
+    }
 }
